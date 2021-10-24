6bebce3b619d0265c381a189c7ade8ba01d9d32a
john horo <johnhoro125@gmail.com>
2021-10-24 22:28:39 +0530

    Disco Grid

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,14 +1,54 @@
-let first = document.querySelector(".first");
-let second = document.querySelector(".second");
-
-function randomColor() {
-  return Math.floor(Math.random() * 16777215).toString(16);
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
 
-first.addEventListener("click", function () {
-  first.style.backgroundColor = "#" + randomColor();
-});
+function generateRandomColor() {
+  let hexCharacters = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
 
-second.addEventListener("mousemove", function () {
-  second.style.backgroundColor = "#" + randomColor();
-});
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = getRandomNumber(16);
+    color = color + hexCharacters[randomNumber];
+  }
+  return color;
+}
+let parentBox = document.querySelector(".boxes");
+
+for (let i = 0; i < 500; i++) {
+  let div = document.createElement(`div`);
+  div.classList.add(`box`);
+  let h3 = document.createElement(`h3`);
+  let randomNo = getRandomNumber(500);
+  h3.innerText = randomNo;
+
+  div.append(h3);
+  parentBox.append(div);
+}
+
+let allBoxes = document.querySelectorAll(".box");
+function handleMouseMove() {
+  allBoxes.forEach((box) => {
+    box.style.backgroundColor = generateRandomColor();
+    box.querySelector("h3").innerText = getRandomNumber(500);
+  });
+}
+
+parentBox.addEventListener("mousemove", handleMouseMove);
